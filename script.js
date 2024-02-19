f221425ae8bde6c85500238eeab8b4ac2a4efaee
onistore <148183467+onistore@users.noreply.github.com>
2024-02-19 19:30:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,97 +6,17 @@ alert ("donate pencet 'DOTANE HERE' diatas . jangan lupa SUBSCRIBE")
 
 confirm ("DI SINI HANYA UPLOAD EPISODE TERBARU SAJA")
 
-const product = [
-  {
-      id: 1,
-      title: 'AGAINST THE SKY SUPREME',
-  },
-  {
-      id: 2,
-      title: 'AGAINST THE GOD',
-  },
-  {
-      id: 3,
-      title: 'APOTHEOSIS',
-  },
-{
-    id: 4,
-    title: 'BATTLE THROUGH THE HEAVENS S5',
-},
-{
-    id: 5,
-    title: 'GOD OF WAR',
-},
-{
-    id: 6,
-    title: 'GREAT DOCTOR MISS NINE',
-},
-{
-  id: 7,
-  title: 'LIAN QI SHI WAN NIAN',
-},
-{
-  id: 8,
-  title: 'MAGIC CHEF OF ICE AND FIRE',
-},
-{
-  id: 9,
-  title: 'PERFECT WORLD',
-},
-{
-  id: 10,
-  title: 'RENEGADE IMMORTAL',
-},
-
-{
-  id: 11,
-  title: 'SOUL LAND 2',
-},
-{
-    id: 12,
-    title: 'SWALLOWED STAR S3',
-},
-{
-  id: 13,
-  title: 'THE GREAT RULER',
-},
-{
-  id: 14,
-  title: 'THRONE OF SEAL',
-},
-{
-  id: 15,
-  title: 'WU DONG QIAN KUN S4',
-},
-{
-  id: 16,
-  title: 'XI XING JI S5',
-},
-];
-
-const categories = [...new Set(product.map((item) => { return item }))]
-
-document.getElementById('searchBar').addEventListener('keyup', (e) => {
-  const searchData = e.target.value.toLowerCase();
-  const filteredData = categories.filter((item) => {
-      return (
-          item.title.toLowerCase().includes(searchData)
-      )
-  })
-  displayItem(filteredData)
-});
-
-const displayItem = (items) => {
-  document.getElementById('root').innerHTML = items.map((item) => {
-      var { image, title } = item;
-      return (
-          ` 
-<div class='bottom'>
-                  <a id="1" href="https://beli.onistore.my.id/back.html"><button><p> ${title}</p></button></a>
-              </div>
-          </div>`
-      )
-  }).join('')
-};
-displayItem(categories);
-
+function search_animal() {
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('animals');
+ 
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    }
+    else {
+      x[i].style.display = "list-item";
+    }
+  }
+}
