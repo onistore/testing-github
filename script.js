f145072d03f07928dadd37377eb71a9ca5bde27e
onistore <148183467+onistore@users.noreply.github.com>
2024-02-21 19:21:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ confirm ("DI SINI HANYA UPLOAD EPISODE TERBARU SAJA")
 function search_animal() {
   let input = document.getElementById('searchbar').value
   input = input.toLowerCase();
-  let x = document.getElementsByClassName('animals');
+  let x = document.getElementsByClassName('donghua');
  
   for (i = 0; i < x.length; i++) {
     if (!x[i].innerHTML.toLowerCase().includes(input)) {
